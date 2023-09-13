ea338f4de90770f92baf24f7944d1931fb64d47e
patrickmurayari <patrickmurayari03@gmail.com>
2023-09-13 17:45:21 -0300

    add files

--- clase2/3.express.js
+++ clase2/3.express.js
@@ -6,23 +6,35 @@ const PORT =  process.env.PORT ?? 1234
 const app = express()
 app.disable('x-powered-by')
 
+app.use(express.json())
+// app.use((req,res, next)=> {
+//     if(req.method !== 'POST') return next()
+//     if(req.headers['content-type'] !== 'aplication/json') return next()
+
+//     //solo llegan request que son POST y que tienen el header Content-Type: application/json
+//     let body = ''
+
+//     //escuchar el evento data 
+//     req.on('data' , chunk => {
+//         body += chunk.toString()
+//     })
+
+//     req.on('end', () => {
+//         const data = JSON.parse(body)
+//         data.timestamp  = Date.now() 
+//         //mutar la request y meter la informacion en el req.body 
+//         req.body = data
+//         next()
+//     })
+// })
+
 app.get('/pokemon/ditto' , (req,res) => {
     res.json(ditto)
 })
 
 app.post('/pokemon' ,(req,res) => {
-    let body = ''
-
-    //escuchar el evento data 
-    req.on('data' , chunk => {
-        body += chunk.toString()
-    })
-
-    req.on('end', () => {
-        const data = JSON.parse(body)
-        data.timestamp  = Date.now()  
-        res.status(201).send(data)
-    })
+    //req.vody deberiamos guardar en bbdd
+    res.status(201).json(req.body)
 })
 
 //LA ULTIMA  A LA QUE VA A LLEGAR
